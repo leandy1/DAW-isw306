738d746b11eaeb0e084b99e0fd1164629826ebee
Gustavo Pascual <elprincipe200229@gmail.com>
2026-06-05 19:36:23 -0400

    Feat: Agregar listado de citas con localStorage

---
+++ citas.js
@@ -0,0 +1,46 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const citas = JSON.parse(localStorage.getItem("Citas")) || [];
+
+    const tabla = document.getElementById("tbody-citas");
+
+    if (!tabla) return;
+
+    citas.forEach((cita, index) => {
+
+        let claseEstado = "";
+
+        if (cita.estado === "Completado") {
+            claseEstado = "activo-estado";
+        } else if (cita.estado === "Pendiente") {
+            claseEstado = "pendiente-estado";
+        } else if (cita.estado === "Esperando Pieza") {
+            claseEstado = "espera-pieza-estado";
+        }
+
+        tabla.innerHTML += `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${cita.nombre} ${cita.apellido}</td>
+                <td>${cita.cedula}</td>
+                <td>${cita.telefono}</td>
+                <td>${cita.marca} ${cita.modelo}</td>
+                <td>${cita.placa}</td>
+                <td>${cita.tipoServicio}</td>
+                <td>${cita.tecnicoAsignado}</td>
+                <td>
+                    <span class="${claseEstado}">
+                        ${cita.estado}
+                    </span>
+                </td>
+                <td>Ver</td>
+            </tr>
+        `;
+    });
+
+    document.getElementById("badge-total").textContent =
+        `${citas.length} citas`;
+
+});
+
