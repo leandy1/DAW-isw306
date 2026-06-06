9ac051feb7fb51179cc625f569d73dc6be346662
reimym132001-bit <reimym132001@gmail.com>
2026-06-06 00:03:43 -0400

    feat:mostrar citas recientes en tiempo real en el dashboard

---
+++ dashboard.js
@@ -0,0 +1,36 @@
+const formulario = document.getElementById("registroForm");
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const tbody = document.querySelector("#ordenesRecientes tbody");
+    const citas = JSON.parse(localStorage.getItem("Citas")) || [];
+
+  
+
+    if (citas.length === 0) {
+        tbody.innerHTML = `<tr><td colspan="5">No hay órdenes registradas</td></tr>`;
+        return;
+    }
+
+    citas.forEach(cita => {
+
+        let claseEstado = "pendiente-estado";
+        if (cita.estado === "Completado")      claseEstado = "activo-estado";
+        if (cita.estado === "Esperando Pieza") claseEstado = "espera-pieza-estado";
+
+        tbody.innerHTML += `
+            <tr>
+                <td>${cita.nombre} ${cita.apellido}</td>
+                <td>${cita.marca} ${cita.modelo}</td>
+                <td>${cita.placa}</td>
+                <td>${cita.tipoServicio}</td>
+                <td><span class="${claseEstado}">${cita.estado}</span></td>
+            </tr>
+        `;
+    });
+
+});
+
+
